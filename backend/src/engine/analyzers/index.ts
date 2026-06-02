836f6866089fa3c4949fa8d616fb5ddc8424750c
UGOD <kabufredrick9@gmail.com>
2026-06-02 04:40:50 +0000

    refactor(backend): Add barrel export for category analyzers

---
+++ backend/src/engine/analyzers/index.ts
@@ -0,0 +1,5 @@
+export { analyzeReentrancy } from "./reentrancy";
+export { analyzeAccessControl } from "./accessControl";
+export { analyzeOverflow } from "./overflow";
+export { analyzeGas } from "./gas";
+export { analyzeLogic } from "./logic";
